ad2cfdda33de7f5723ed6cadd8318ffc74e9760c
MubarratHossain <tahsinezab97@gmail.com>
2024-11-03 20:58:16 +0600

    Setting up React router dom environment with tailwind and daisyui ,  A navbar created with banner according to figma , next - A Responsive Footer

---
+++ Gadget-Heaven/src/components/Banner/Banner.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import bannerImage from './banner.jpg';
+
+const Banner = () => {
+    return (
+        <div className="relative text-center mt-12">
+            <h3 className="text-2xl font-bold">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h3>
+            <p className="mt-4">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
+            <button className="mt-6 mb-4 px-6 py-2 bg-white text-[#9538E2] rounded-full font-semibold shadow-md 
+               transition duration-300 ease-in-out transform hover:bg-[#f3f4f6] hover:scale-105 focus:outline-none focus:ring-2 focus:ring-[#b4a4c2] focus:ring-opacity-50">
+                Shop Now
+            </button>
+
+            <img
+                src={bannerImage}
+                alt="Gadget Accessory"
+                className="absolute left-1/2 transform -translate-x-1/2 -translate-y-10 w-[50%] mt-12 rounded-2xl border-4 border-white ring-2 ring-[#9538E2] transition-all duration-300 ease-in-out hover:ring-4"
+            />
+        </div>
+
+    );
+};
+
+export default Banner;
---
+++ Gadget-Heaven/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Banner from '../Banner/Banner';
+
+
+const Navbar = () => {
+    const location = useLocation();
+
+    return (
+        <div className="bg-gradient-to-r from-[#9538E2] to-[#7C29B3] m-4 p-4 rounded-xl text-white h-[500px]">
+          
+            <div className="navbar">
+                <div className="navbar-start">
+                   
+                    <a className="btn btn-ghost text-xl text-white">Gadget Heaven</a>
+                </div>
+
+                <div className="navbar-center hidden lg:flex">
+                    
+                    <ul className="menu menu-horizontal p-0 text-white">
+                        <li><a>Homepage</a></li>
+                        <li><a>Statistics</a></li>
+                        <li><a>Dashboard</a></li>
+                    </ul>
+                </div>
+
+                <div className="navbar-center lg:hidden">
+                    
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 text-white"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h7" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li><a>Homepage</a></li>
+                            <li><a>Statistics</a></li>
+                            <li><a>Dashboard</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="navbar-end gap-4">
+                   
+                    <button className="btn btn-ghost btn-circle bg-white">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5 text-black"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M3 3h2l.4 2M7 13h10l1.6-8H6.4L5.2 4H3m3 9l-1 5h14l-1-5M5 21a2 2 0 100-4 2 2 0 000 4zm12 0a2 2 0 100-4 2 2 0 000 4z" />
+                        </svg>
+                    </button>
+
+                   
+                    <button className="btn btn-ghost btn-circle bg-white">
+                        <div className="indicator">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 text-black"
+                                fill="white"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4.318 6.318a4.5 4.5 0 016.364 0L12 7.636l1.318-1.318a4.5 4.5 0 116.364 6.364L12 20.364l-7.682-7.682a4.5 4.5 0 010-6.364z" />
+                            </svg>
+                        </div>
+                    </button>
+                </div>
+            </div>
+
+            
+            {location.pathname === '/' && <Banner></Banner>}
+        </div>
+    );
+};
+
+export default Navbar;
