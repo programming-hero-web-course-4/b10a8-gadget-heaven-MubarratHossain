8fa3e51db940317549480ee0bc07c445d7f64518
MubarratHossain <tahsinezab97@gmail.com>
2024-11-06 22:12:28 +0600

    Added a window.scrollTo top for view details button

--- Gadget-Heaven/src/components/PhotoCard/Photocards.jsx
+++ Gadget-Heaven/src/components/PhotoCard/Photocards.jsx
@@ -7,6 +7,12 @@ const Photocards = ({ item }) => {
 
     const handleViewDetails = () => {
         navigate(`/product/${item?.product_id}`);
+        setTimeout(() => {
+            window.scrollTo({
+                top: 0,  
+                behavior: 'smooth',
+            });
+        }, 100);
     };
 
     return (
